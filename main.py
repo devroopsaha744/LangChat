e1adfe69dcb5d4b9e993ee9d7bce092d999a73df
Devroop Saha <130696540+devroopsaha744@users.noreply.github.com>
2024-06-07 17:51:55 +0530

    main app v01

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st 
+from querying import generate_response
+
+st.title("LangChat")
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Chat with Langchain docs"}]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if prompt := st.chat_input("Chat with langchain docs "):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = generate_response(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
